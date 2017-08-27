3dd51b05b472b8febdbb37099204aa7b033c436e
Ray Britton <raybritton@gmail.com>
2017-08-27 07:59:34 +0100

    Change json query to handle any object as the json could be an array or object

--- lib/src/main/kotlin/com/raybritton/jsonquery/JsonQuery.kt
+++ lib/src/main/kotlin/com/raybritton/jsonquery/JsonQuery.kt
@@ -3,24 +3,23 @@ package com.raybritton.jsonquery
 import com.google.gson.Gson
 import com.raybritton.jsonquery.models.Query
 import com.raybritton.jsonquery.utils.describe
+import com.raybritton.jsonquery.utils.filter
 import com.raybritton.jsonquery.utils.navigate
 
 class JsonQuery {
-    private lateinit var map: Map<String, Any>
+    private lateinit var jsonObj: Any
 
     fun loadJson(path: String) {
         val json = JsonLoader().load(path)
         @Suppress("UNCHECKED_CAST") //This is ok as long as Gson doesn't change it's implementation
-        map = Gson().fromJson(json, Any::class.java) as Map<String, Any>
-        if (map.javaClass.typeName != "com.google.gson.internal.LinkedTreeMap") {
-            throw IllegalStateException("Unable to parse json")
-        }
+        jsonObj = Gson().fromJson(json, Any::class.java)
     }
 
     fun query(queryStr: String): String {
         val query = queryStr.toQuery()
+        val filtered = jsonObj.filter(query)
         when (query.method) {
-            Query.Method.DESCRIBE -> return map.navigate(query.target).describe()
+            Query.Method.DESCRIBE -> return filtered.navigate(query.target).describe()
             Query.Method.LIST -> TODO()
             Query.Method.GET -> TODO()
         }
