ffb3496058487cc55a597b27b95e130382c73b5a
Ray Britton <raybritton@gmail.com>
2017-08-28 09:11:55 +0100

    Add support for urls, print errors

--- lib/src/main/kotlin/com/raybritton/jsonquery/JsonLoader.kt
+++ lib/src/main/kotlin/com/raybritton/jsonquery/JsonLoader.kt
@@ -1,19 +1,29 @@
 package com.raybritton.jsonquery
 
 import java.io.File
+import java.net.URL
+import kotlin.streams.toList
 
 internal class JsonLoader() {
     fun load(input: String): String {
         val input = input.trim()
         when {
             input.startsWith("{") -> { return input }
-            input.startsWith("file") -> { return loadFromFile(input) }
-            else -> throw IllegalStateException("$input is not supported")
+            input.startsWith("http") -> { return loadFromUrl(input) }
+            else -> { return loadFromFile(input) }
         }
     }
 
     private fun loadFromFile(path: String): String {
         val file = File(path)
         return file.readLines().joinToString("")
+    }
+
+    private fun loadFromUrl(url: String): String {
+        return URL(url).openStream()
+                .bufferedReader()
+                .lines()
+                .toList()
+                .joinToString("")
     }
 }
