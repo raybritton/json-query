64a6f9e779d077e533e322c385ed0314389b2bc0
Ray Britton <raybritton@gmail.com>
2017-08-28 15:49:30 +0100

    Fix readme and imports

--- lib/src/main/kotlin/com/raybritton/jsonquery/JsonQuery.kt
+++ lib/src/main/kotlin/com/raybritton/jsonquery/JsonQuery.kt
@@ -1,6 +1,5 @@
 package com.raybritton.jsonquery
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.raybritton.jsonquery.models.Query
 import com.raybritton.jsonquery.printer.describe
