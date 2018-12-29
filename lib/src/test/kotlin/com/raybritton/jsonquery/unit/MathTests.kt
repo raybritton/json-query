421280824925845c4851d8a271621000340a9313
Ray Britton <raybritton@gmail.com>
2018-12-29 17:33:35 +0000

    Add more tests for maths
    This include tests where the arrays aren't just numbers

--- lib/src/test/kotlin/com/raybritton/jsonquery/unit/MathTests.kt
+++ lib/src/test/kotlin/com/raybritton/jsonquery/unit/MathTests.kt
@@ -2,6 +2,7 @@ package com.raybritton.jsonquery.unit
 
 import com.raybritton.jsonquery.models.ElementFieldProjection
 import com.raybritton.jsonquery.models.JsonArray
+import com.raybritton.jsonquery.models.JsonObject
 import com.raybritton.jsonquery.parsing.tokens.Keyword
 import com.raybritton.jsonquery.tools.math
 import org.junit.Assert.assertEquals
@@ -9,7 +10,7 @@ import org.junit.Test
 
 class MathTests {
     @Test
-    fun testAllPositiveMin() {
+    fun `test minimum for all positive array`() {
         val array = JsonArray(10, 20, 23, 25)
 
         val min = array.math(Keyword.MIN, ElementFieldProjection.Element)
@@ -18,7 +19,7 @@ class MathTests {
     }
 
     @Test
-    fun testAllNegativeMin() {
+    fun `test minimum for all negative array`() {
         val array = JsonArray(-10, -5, -1)
 
         val min = array.math(Keyword.MIN, ElementFieldProjection.Field(""))
@@ -27,7 +28,7 @@ class MathTests {
     }
 
     @Test
-    fun testMixedMin() {
+    fun `test minimum for mixed sign array`() {
         val array = JsonArray(-10.56, -5, 0, 1)
 
         val min = array.math(Keyword.MIN, ElementFieldProjection.Field(""))
@@ -36,7 +37,16 @@ class MathTests {
     }
 
     @Test
-    fun testAllPositiveMax() {
+    fun `test minimum for mixed array`() {
+        val array = JsonArray(-10.56, -5, 0, JsonObject("not" to "this"))
+
+        val min = array.math(Keyword.MIN, ElementFieldProjection.Field(""))
+
+        assertEquals("min", -10.56, min, 0.0)
+    }
+
+    @Test
+    fun `test maximum for all positive array`() {
         val array = JsonArray(10, 20, 23, 25)
 
         val max = array.math(Keyword.MAX, ElementFieldProjection.Element)
@@ -45,7 +55,7 @@ class MathTests {
     }
 
     @Test
-    fun testAllNegativeMax() {
+    fun `test maximum for all negative array`() {
         val array = JsonArray(-10, -5, -1)
 
         val max = array.math(Keyword.MAX, ElementFieldProjection.Field(""))
@@ -54,7 +64,7 @@ class MathTests {
     }
 
     @Test
-    fun testMixedMax() {
+    fun `test maximum for mixed sign array`() {
         val array = JsonArray(-10.56, -5, 0, 1)
 
         val max = array.math(Keyword.MAX, ElementFieldProjection.Field(""))
@@ -63,7 +73,16 @@ class MathTests {
     }
 
     @Test
-    fun testAllPositiveSum() {
+    fun `test maximum for mixed array`() {
+        val array = JsonArray(-10.56, -5, 0, true)
+
+        val max = array.math(Keyword.MAX, ElementFieldProjection.Field(""))
+
+        assertEquals("max", 0.0, max, 0.0)
+    }
+
+    @Test
+    fun `test sum for all positive array`() {
         val array = JsonArray(10, 20, 23, 25)
 
         val sum = array.math(Keyword.SUM, ElementFieldProjection.Element)
@@ -72,7 +91,7 @@ class MathTests {
     }
 
     @Test
-    fun testAllNegativeSum() {
+    fun `test sum for all negative array`() {
         val array = JsonArray(-10, -5, -1)
 
         val sum = array.math(Keyword.SUM, ElementFieldProjection.Field(""))
@@ -81,7 +100,7 @@ class MathTests {
     }
 
     @Test
-    fun testMixedSum() {
+    fun `test sum for mixed sign array`() {
         val array = JsonArray(-10.56, -5, 0, 1)
 
         val sum = array.math(Keyword.SUM, ElementFieldProjection.Field(""))
@@ -90,7 +109,16 @@ class MathTests {
     }
 
     @Test
-    fun testEmptyArrayCount() {
+    fun `test sum for mixed array`() {
+        val array = JsonArray(-10.56, -5, 0, "hello")
+
+        val sum = array.math(Keyword.SUM, ElementFieldProjection.Field(""))
+
+        assertEquals("max", -15.56, sum, 0.0)
+    }
+
+    @Test
+    fun `test count for empty array`() {
         val array = JsonArray()
 
         val count = array.math(Keyword.COUNT, ElementFieldProjection.Element)
@@ -99,11 +127,65 @@ class MathTests {
     }
 
     @Test
-    fun testArrayCount() {
+    fun `test count for array of mixed sign numbers`() {
         val array = JsonArray(-10.56, -5, 0, 1)
 
         val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
 
         assertEquals("count", 4.0, count, 0.0)
+    }
+
+    @Test
+    fun `test count for array of words`() {
+        val array = JsonArray("test", "word")
+
+        val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
+
+        assertEquals("count", 2.0, count, 0.0)
+    }
+
+    @Test
+    fun `test count for array of booleans`() {
+        val array = JsonArray(true, false, true)
+
+        val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
+
+        assertEquals("count", 3.0, count, 0.0)
+    }
+
+    @Test
+    fun `test count for array of nulls`() {
+        val array = JsonArray(null, null, null, null)
+
+        val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
+
+        assertEquals("count", 4.0, count, 0.0)
+    }
+
+    @Test
+    fun `test count for array of objects`() {
+        val array = JsonArray(JsonObject(), JsonObject("foo" to "bar"))
+
+        val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
+
+        assertEquals("count", 2.0, count, 0.0)
+    }
+
+    @Test
+    fun `test count for array of arrays`() {
+        val array = JsonArray(JsonArray(), JsonArray(1, 2, 3))
+
+        val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
+
+        assertEquals("count", 2.0, count, 0.0)
+    }
+
+    @Test
+    fun `test count for array of mixed`() {
+        val array = JsonArray("test", true, null, JsonObject(), JsonArray(6, 7, 8), 1)
+
+        val count = array.math(Keyword.COUNT, ElementFieldProjection.Field(""))
+
+        assertEquals("count", 6.0, count, 0.0)
     }
 }
