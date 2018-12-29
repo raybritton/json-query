fae4f4151467a5e25a741666ff27360e8d082746
Ray Britton <raybritton@gmail.com>
2018-12-29 18:26:22 +0000

    Add tests for contains, not contains, equal and not equal operators

---
+++ lib/src/test/kotlin/com/raybritton/jsonquery/unit/operators/ContainsOperatorTests.kt
@@ -0,0 +1,77 @@
+package com.raybritton.jsonquery.unit.operators
+
+import com.raybritton.jsonquery.models.JsonArray
+import com.raybritton.jsonquery.models.JsonObject
+import com.raybritton.jsonquery.models.Value
+import com.raybritton.jsonquery.parsing.tokens.Operator
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ContainsOperatorTests {
+    @Test
+    fun `test contains in string`() {
+        val operator = Operator.Contains
+
+        assertTrue("string in string - not checking case", operator.op("this is a string", Value.ValueString("this"), false))
+        assertFalse("string not in string - not checking case", operator.op("this is a string", Value.ValueString("not"), false))
+
+        assertTrue("string in string - checking case", operator.op("this is a string", Value.ValueString("this"), true))
+        assertFalse("string not in string - checking case", operator.op("this is a string", Value.ValueString("not"), true))
+
+        assertFalse("string in string (diff case) - checking case", operator.op("this is a string", Value.ValueString("THIS"), true))
+        assertFalse("string not in string (diff case) - checking case", operator.op("this is a string", Value.ValueString("NOT"), true))
+
+        assertTrue("string in string (diff case) - not checking case", operator.op("this is a string", Value.ValueString("THIS"), false))
+        assertFalse("string not in string (diff case) - not checking case", operator.op("this is a string", Value.ValueString("NOT"), false))
+
+        assertTrue("whole string match", operator.op("string", Value.ValueString("string"), false))
+    }
+
+    @Test
+    fun `test contains in array`() {
+        val operator = Operator.Contains
+
+        assertTrue("string in array - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("this"), false))
+        assertFalse("string not in array - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("not"), false))
+
+        assertTrue("string in array - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("this"), true))
+        assertFalse("string not in array - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("not"), true))
+
+        assertTrue("string in array (diff case) - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("THIS"), false))
+        assertFalse("string not in array (diff case) - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("NOT"), false))
+
+        assertFalse("string in array (diff case) - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("THIS"), true))
+        assertFalse("string not in array (diff case) - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("NOT"), true))
+
+        assertTrue("integer in array", operator.op(JsonArray(1, 2, 3), Value.ValueNumber(1.0), false))
+        assertFalse("integer not in array", operator.op(JsonArray(1, 2, 3), Value.ValueNumber(4.0), false))
+
+        assertTrue("double in array", operator.op(JsonArray(1.3, 2.2, 3.1), Value.ValueNumber(1.3), false))
+        assertFalse("double not in array", operator.op(JsonArray(1.3, 2.2, 3.1), Value.ValueNumber(1.5), false))
+
+        assertTrue("boolean in array", operator.op(JsonArray(true, false), Value.ValueBoolean(true), false))
+        assertFalse("boolean not in array", operator.op(JsonArray(true), Value.ValueBoolean(false), false))
+
+        assertTrue("null in array", operator.op(JsonArray(true, null), Value.ValueNull, false))
+        assertFalse("null not in array", operator.op(JsonArray("not null"), Value.ValueNull, false))
+    }
+
+    @Test
+    fun `test contains in object`() {
+        val operator = Operator.Contains
+
+        assertTrue("matching key, matching case - not checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("foo"), false))
+        assertTrue("matching key, matching case - checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("foo"), true))
+
+        assertTrue("matching key, not matching case - not checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("FOO"), false))
+        assertFalse("matching key, not matching case - checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("FOO"), true))
+
+        assertFalse("not matching key - not checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("bar"), false))
+        assertFalse("not matching key - checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("bar"), true))
+
+        assertTrue("boolean key", operator.op(JsonObject("true" to "bar"), Value.ValueBoolean(true), false))
+
+        assertTrue("number key", operator.op(JsonObject("123" to "bar"), Value.ValueNumber(123.0), false))
+    }
+}
---
+++ lib/src/test/kotlin/com/raybritton/jsonquery/unit/operators/NotContainsOperatorTests.kt
@@ -0,0 +1,77 @@
+package com.raybritton.jsonquery.unit.operators
+
+import com.raybritton.jsonquery.models.JsonArray
+import com.raybritton.jsonquery.models.JsonObject
+import com.raybritton.jsonquery.models.Value
+import com.raybritton.jsonquery.parsing.tokens.Operator
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class NotContainsOperatorTests {
+    @Test
+    fun `test contains in string`() {
+        val operator = Operator.NotContains
+
+        assertFalse("string in string - not checking case", operator.op("this is a string", Value.ValueString("this"), false))
+        assertTrue("string not in string - not checking case", operator.op("this is a string", Value.ValueString("not"), false))
+
+        assertFalse("string in string - checking case", operator.op("this is a string", Value.ValueString("this"), true))
+        assertTrue("string not in string - checking case", operator.op("this is a string", Value.ValueString("not"), true))
+
+        assertTrue("string in string (diff case) - checking case", operator.op("this is a string", Value.ValueString("THIS"), true))
+        assertTrue("string not in string (diff case) - checking case", operator.op("this is a string", Value.ValueString("NOT"), true))
+
+        assertFalse("string in string (diff case) - not checking case", operator.op("this is a string", Value.ValueString("THIS"), false))
+        assertTrue("string not in string (diff case) - not checking case", operator.op("this is a string", Value.ValueString("NOT"), false))
+
+        assertFalse("whole string match", operator.op("string", Value.ValueString("string"), false))
+    }
+
+    @Test
+    fun `test contains in array`() {
+        val operator = Operator.NotContains
+
+        assertFalse("string in array - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("this"), false))
+        assertTrue("string not in array - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("not"), false))
+
+        assertFalse("string in array - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("this"), true))
+        assertTrue("string not in array - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("not"), true))
+
+        assertFalse("string in array (diff case) - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("THIS"), false))
+        assertTrue("string not in array (diff case) - not checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("NOT"), false))
+
+        assertTrue("string in array (diff case) - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("THIS"), true))
+        assertTrue("string not in array (diff case) - checking case", operator.op(JsonArray("this", "is", "string"), Value.ValueString("NOT"), true))
+
+        assertFalse("integer in array", operator.op(JsonArray(1, 2, 3), Value.ValueNumber(1.0), false))
+        assertTrue("integer not in array", operator.op(JsonArray(1, 2, 3), Value.ValueNumber(4.0), false))
+
+        assertFalse("double in array", operator.op(JsonArray(1.3, 2.2, 3.1), Value.ValueNumber(1.3), false))
+        assertTrue("double not in array", operator.op(JsonArray(1.3, 2.2, 3.1), Value.ValueNumber(1.5), false))
+
+        assertFalse("boolean in array", operator.op(JsonArray(true, false), Value.ValueBoolean(true), false))
+        assertTrue("boolean not in array", operator.op(JsonArray(true), Value.ValueBoolean(false), false))
+
+        assertFalse("null in array", operator.op(JsonArray(true, null), Value.ValueNull, false))
+        assertTrue("null not in array", operator.op(JsonArray("not null"), Value.ValueNull, false))
+    }
+
+    @Test
+    fun `test contains in object`() {
+        val operator = Operator.NotContains
+
+        assertFalse("matching key, matching case - not checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("foo"), false))
+        assertFalse("matching key, matching case - checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("foo"), true))
+
+        assertFalse("matching key, not matching case - not checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("FOO"), false))
+        assertTrue("matching key, not matching case - checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("FOO"), true))
+
+        assertTrue("not matching key - not checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("bar"), false))
+        assertTrue("not matching key - checking case", operator.op(JsonObject("foo" to "bar"), Value.ValueString("bar"), true))
+
+        assertFalse("boolean key", operator.op(JsonObject("true" to "bar"), Value.ValueBoolean(true), false))
+
+        assertFalse("number key", operator.op(JsonObject("123" to "bar"), Value.ValueNumber(123.0), false))
+    }
+}
