13766b4a7b74bb9024fa1b728d5d4476dfc97ede
Ray Britton <raybritton@gmail.com>
2018-12-30 09:01:25 +0000

    Convert Gson data structure to JQL data structures

--- lib/src/main/kotlin/com/raybritton/jsonquery/models/JsonObject.kt
+++ lib/src/main/kotlin/com/raybritton/jsonquery/models/JsonObject.kt
@@ -1,9 +1,19 @@
 package com.raybritton.jsonquery.models
 
+import com.google.gson.internal.LinkedTreeMap
+
 class JsonObject : HashMap<String, Any?> {
     constructor() : super()
     constructor(collection: Map<String, Any?>) : super(collection)
     constructor(vararg content: Pair<String, Any?>) : super(mapOf(*content))
 
     fun copy() = JsonObject(this)
+
+    companion object {
+        fun fromLinkedTreeMap(map: LinkedTreeMap<*, *>): JsonObject {
+            val obj = JsonObject()
+            map.forEach { (key, value) -> obj[key.toString()] = value }
+            return obj
+        }
+    }
 }
