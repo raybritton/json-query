37adbacf92836db7ac484a20f0fc15926c6819cc
Ray Britton <raybritton@gmail.com>
2018-12-31 08:03:59 +0000

    Move query printing to separate class it's long

---
+++ lib/src/main/kotlin/com/raybritton/jsonquery/printers/QueryPrinter.kt
@@ -0,0 +1,113 @@
+package com.raybritton.jsonquery.printers
+
+import com.raybritton.jsonquery.ext.wrap
+import com.raybritton.jsonquery.models.*
+import com.raybritton.jsonquery.parsing.tokens.Keyword
+
+internal object QueryPrinter {
+    fun print(query: Query): String {
+        fun StringBuilder.appendKey(keyword: Keyword) = append(' ').append(keyword.name)
+
+        val builder = StringBuilder()
+        when (query.method) {
+            Query.Method.SEARCH -> {
+                builder.appendKey(Keyword.SEARCH).append(' ')
+                if (query.flags.isDistinct) builder.appendKey(Keyword.DISTINCT).append(' ')
+                builder.append(query.target).append(' ')
+                        .appendKey(Keyword.FOR).append(' ')
+                        .append(query.search!!.operator.symbol).append(' ')
+                if (query.search.value is Value.ValueQuery) {
+                    builder.append('(')
+                }
+                builder.append(query.search.value)
+                if (query.search.value is Value.ValueQuery) {
+                    builder.append(')')
+                }
+                if (query.flags.isCaseSensitive) builder.appendKey(Keyword.CASE).appendKey(Keyword.SENSITIVE)
+                if (query.flags.isWithValues) builder.appendKey(Keyword.WITH).appendKey(Keyword.VALUES)
+            }
+            Query.Method.SELECT -> {
+                builder.appendKey(Keyword.SELECT).append(' ')
+                if (query.flags.isDistinct) builder.appendKey(Keyword.DISTINCT).append(' ')
+                if (query.select!!.projection != null && query.select.projection !is SelectProjection.All) {
+                    builder.append(printSelectProjection(query.select.projection!!))
+                            .appendKey(Keyword.FROM).append(' ')
+                }
+                builder.append(query.target).append(' ')
+                if (query.flags.isByElement) builder.appendKey(Keyword.BY).appendKey(Keyword.ELEMENT).append(' ')
+                if (query.where != null) builder.append(printWhere(query.where))
+                if (query.flags.isCaseSensitive) builder.appendKey(Keyword.CASE).appendKey(Keyword.SENSITIVE)
+                if (query.select.limit != null) builder.appendKey(Keyword.LIMIT).append(' ').append(query.select.limit)
+                if (query.select.offset != null) builder.appendKey(Keyword.OFFSET).append(' ').append(query.select.offset)
+                if (query.flags.isAsJson) builder.appendKey(Keyword.AS).appendKey(Keyword.JSON)
+                if (query.flags.isPrettyPrinted) builder.appendKey(Keyword.PRETTY)
+
+            }
+            Query.Method.DESCRIBE -> {
+                builder.appendKey(Keyword.DESCRIBE).append(' ')
+                if (query.flags.isDistinct) builder.appendKey(Keyword.DISTINCT).append(' ')
+                if (query.describe!!.projection != null) {
+                    builder.append(query.describe.projection.wrap())
+                            .appendKey(Keyword.FROM).append(' ')
+                }
+                builder.append(query.target).append(' ')
+                if (query.where != null) builder.append(printWhere(query.where))
+                if (query.flags.isCaseSensitive) builder.append(' ').appendKey(Keyword.CASE).appendKey(Keyword.SENSITIVE)
+                if (query.describe.limit != null) builder.appendKey(Keyword.LIMIT).append(' ').append(query.describe.limit)
+                if (query.describe.offset != null) builder.appendKey(Keyword.OFFSET).append(' ').append(query.describe.offset)
+                if (query.flags.isPrettyPrinted) builder.appendKey(Keyword.PRETTY)
+            }
+        }
+        return builder.toString()
+    }
+
+    private fun printWhere(where: Where): String {
+        val builder = StringBuilder()
+
+        when (where.projection) {
+            is WhereProjection.Field -> builder.append(where.projection.value.wrap())
+            WhereProjection.Element -> builder.append(Keyword.ELEMENT.name)
+            is WhereProjection.Math -> {
+                builder.append(where.projection.expr)
+                builder.append('(')
+                when (where.projection.field) {
+                    is ElementFieldProjection.Field -> builder.append(where.projection.field.value.wrap())
+                    ElementFieldProjection.Element -> builder.append(Keyword.ELEMENT.name)
+                }
+                builder.append(')')
+            }
+        }
+
+        builder.append(' ')
+
+        builder.append(where.operator.symbol)
+
+        builder.append(' ')
+
+        builder.append(where.value.value)
+
+        return builder.toString()
+    }
+
+    private fun printSelectProjection(selectProjection: SelectProjection): String {
+        val builder = StringBuilder()
+
+        when (selectProjection) {
+            is SelectProjection.SingleField -> builder.append(selectProjection.field)
+            is SelectProjection.MultipleFields -> builder.append(selectProjection.fields.joinToString(", ", prefix = "(", postfix = ")"))
+            is SelectProjection.Math -> {
+                builder.append(selectProjection.expr)
+                builder.append('(')
+                when (selectProjection.field) {
+                    is ElementFieldProjection.Field -> builder.append(selectProjection.field.value.wrap())
+                    ElementFieldProjection.Element -> builder.append(Keyword.ELEMENT.name)
+                }
+                builder.append(')')
+            }
+            else -> {
+            }
+        }
+
+        return builder.toString()
+    }
+}
